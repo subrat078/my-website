f472a4c3566ccb44735e5de6378d8d22fcaaf727
subrat078 <141222956+subrat078@users.noreply.github.com>
2024-11-15 11:49:53 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,5 @@
+function revealMessage() {
+    const specialNote = document.getElementById('special-note');
+    specialNote.textContent = "You are the best sister anyone could ask for, and I am so lucky to have you.";
+    specialNote.style.display = "block";
+}
